59826d698cbd3a5ed28501824c82ed20616ab47a
Lussati Santos <lussatisantos10@gmail.com>
2024-12-06 22:01:00 +0100

    expanded and short img code

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,15 @@ void main() {
 class Dadoos extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      children: [
+        Expanded(
+          child: Image.asset('imgs/dado1.png'),
+          ),
+        Expanded(
+          child: Image.asset('imgs/dado2.png'),
+        )
+      ],
+    );
   }
 }
